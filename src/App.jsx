9f392cd3c2a4d76804986cb355a497b729759d58
daimyo-rb <daimyo@roshibros.com>
2025-06-21 08:33:29 -0500

    Create basic ExperienceCard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './styles/App.css'
 import { PersonalInfoCard } from './components/PersonalInfoCard'
 import { EducationCard } from './components/EducationCard'
+import { ExperienceCard } from './components/ExperienceCard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>CV Builder</h1>
       <PersonalInfoCard/>
       <EducationCard/>
+      <ExperienceCard/>
     </>
   )
 }
---
+++ src/components/ExperienceCard.jsx
@@ -0,0 +1,16 @@
+import '../styles/ExperienceCard.css'
+
+export function ExperienceCard() {
+  return <div className='experienceCard'>
+    <h2>Experience</h2>
+    <div>
+      <div className='splitLine'>
+        <p>Name</p>
+        <p>Date</p>
+      </div>
+      <ul>
+        <li>First bullet</li>
+      </ul>
+    </div>
+  </div>
+}
