705fa1f078017ce3eb661e4bec429b9f661c6c54
daimyo-rb <daimyo@roshibros.com>
2025-06-21 20:12:08 -0500

    Clean up unused files and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './styles/App.css'
 import { PersonalInfoCard } from './components/PersonalInfoCard'
 import { EducationCard } from './components/EducationCard'
